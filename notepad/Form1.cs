ec2b20bf07e57bb73c459e3c9cb5625b3dea870a
Garuka404 <garukagimhana@gmail.com>
2024-11-10 21:35:54 +0530

    Create Notepad

---
+++ notepad/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace notepad
+{
+    public partial class Form1 : Form
+    {
+        String path = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void newToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            path=String.Empty;
+            textBox1.Text=string.Empty;
+        }
+
+       
+    }
+    }
