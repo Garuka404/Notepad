bc2993d058172a896e718789306eb608ffe80435
Garuka404 <garukagimhana@gmail.com>
2024-11-12 22:18:23 +0530

    Added New, Open, Save, Save As, and Exit options in the menu strip. New clears text and resets the file path. Open loads text from a selected file into textBox1. Save saves text to an existing file or prompts for a new file if no path is set. Save As allows saving text to a new file with a selected path. Exit closes the application. Added error handling for file operations.

--- notepad/Form1.cs
+++ notepad/Form1.cs
@@ -30,7 +30,135 @@ namespace notepad
             path=String.Empty;
             textBox1.Text=string.Empty;
         }
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog obj1 = new OpenFileDialog()
+            {
+                Filter = "Text Documents|*.txt",
+                ValidateNames = true,
+                Multiselect = false
 
-       
+            })
+
+            {
+                if (obj1.ShowDialog() == DialogResult.OK)// user select
+                {
+                    try
+                    {
+                        using (StreamReader sr = new StreamReader(obj1.FileName))
+                        {
+                            path = obj1.FileName;
+                            Task<String> text = sr.ReadLineAsync();
+                            textBox1.Text = text.Result;
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                using (SaveFileDialog obj1 = new SaveFileDialog()
+                {
+                    Filter = "Text Document|*.txt",
+                    ValidateNames = true
+
+                })
+
+                {
+                    if (obj1.ShowDialog() == DialogResult.OK)
+                    {
+                        {
+                            try
+                            {
+                                path = obj1.FileName;
+                                using (StreamWriter sw = new StreamWriter(obj1.FileName))
+
+                                {
+                                    MessageBox.Show(obj1.FileName);
+                                    sw.WriteLine(textBox1.Text);
+
+
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                            }
+                        }
+                    }
+                    else
+                    {
+                        try
+                        {
+                            using (StreamWriter sw = new StreamWriter(path))
+                            {
+                                sw.WriteAsync(textBox1.Text);
+
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+
+        }
+
+        private void saveAsToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            {
+
+                using (SaveFileDialog obj1 = new SaveFileDialog()
+                {
+                    Filter = "Text Document|*.txt",
+                    ValidateNames = true
+
+                })
+
+                {
+                    if (obj1.ShowDialog() == DialogResult.OK)
+                    {
+                        {
+                            try
+                            {
+                                path = obj1.FileName;
+                                using (StreamWriter sw = new StreamWriter(obj1.FileName))
+
+                                {
+                                    MessageBox.Show(obj1.FileName);
+                                    sw.WriteLine(textBox1.Text);
+
+
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
     }
     }
